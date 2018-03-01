f639efd07dfe1b87719dca9b96261abd54f138ac
Sweenu <brunoinec@gmail.com>
2018-03-01 15:47:52 +0100

    details.php's controller

--- src/controllers/details.php
+++ src/controllers/details.php
@@ -1,3 +1,13 @@
 <?php
-$id_film_choisi = $_GET['id'];
+require_once(PATH_MODELS.'GenreDAO.php');
+require_once(PATH_MODELS.'FilmDAO.php');
+
+$gDAO = new genreDAO();
+$fDAO = new genreDAO();
+$id_film_choisi = isset($_GET['id'] ? $_GET['id'] : 1);
+
+$film = $fDAO->fromId($id_film_choisi);
+$genre_libelle = $gDAO->fromId($film->getId());
+
+require_once(PATH_VIEWS.'index.php');
 ?>
